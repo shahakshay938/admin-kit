b5e55ce5512506d2c8a08d966ae3b4ad01529969
Akshay Shah <shahakshay938@gmail.com>
2023-12-08 22:56:16 +0530

    add new traits to core module and removed from stubs

--- src/Core/Traits/Api/ChecksumTrait.php
+++ src/Core/Traits/Api/ChecksumTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Traits\Api;
+namespace Tutus\Adminkit\Core\Traits\Api;
 
 use Carbon\Carbon;
 use Illuminate\Support\Str;
--- src/Core/Traits/InteractWithFiles.php
+++ src/Core/Traits/InteractWithFiles.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Traits;
+namespace Tutus\Adminkit\Core\Traits;
 
 use Illuminate\Support\Str;
 use Illuminate\Foundation\Http\FormRequest;
